a953b11a3c1e30a831300441aa9136ee9ae64578
Heather Kimbley <heather.kimbley@gmail.com>
2023-09-10 18:14:15 -0500

    Update iframe.php

--- iframe.php
+++ iframe.php
@@ -1,1 +1,1 @@
-<!DOCTYPE html><html><?php echo file_get_contents($_REQUEST['https://www.besthf.com/best/Furniture/Product-Customizer-Home']); ?></html>
+<!DOCTYPE html><html><?php echo file_get_contents($_REQUEST['url']); ?></html>
